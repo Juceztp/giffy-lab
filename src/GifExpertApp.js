f452ad611eefc748c8be37ac363040f33fc25a6e
Julio Peña <julio@moonkeys.io>
2020-11-24 18:53:18 -0300

    Initial commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { CategoryAdd } from './components/CategoryAdd';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['One Punch']);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr />
+
+            <CategoryAdd setCategories={ setCategories } />
+
+            <ol>
+                {
+                    categories.map(category => {
+                        return <GifGrid key={ category } category={ category } />
+                    })
+                }
+            </ol>
+        </>
+    )
+}
